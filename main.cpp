6e224452a07f120ea1b5aeb45017399ca0ea38bc
Doug Brummell <doug.brummell@gmail.com>
2016-08-07 21:16:54 -0400

    init

--- main.cpp
+++ main.cpp
@@ -1,31 +1,34 @@
-// Convert this program to C++
-// change to C++ io
-// change to one line comments
-// change defines of constants to const
-// change array to vector<>
-// inline any short function
+// Convert this program to C++ - CHECK
+// change to C++ io - CHECK
+// change to one line comments - CHECK
+// change defines of constants to const - CHECK
+// change array to vector<> - CHECK
+// inline any short function - CHECK
 
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-const int N {40};
-
 template <typename T>
-inline T sum(const vector<T> summable) {
+inline T sum(const vector<T>& summable) {
     T accum = 0;
-    for (T i : summable)
+    for (const auto& i : summable) {
         accum += i;
+    }
     return accum;
 }
 
 int main() {
+    const int N {40};
+
     vector<int> data;
     data.reserve(N);
 
-    for (int i {0}; i < N; ++i)
+    for (int i {0}; i < N; ++i) {
         data.push_back(i);
+    }
+
     auto accum = sum(data);
 
     cout << "sum is " << accum << endl;
